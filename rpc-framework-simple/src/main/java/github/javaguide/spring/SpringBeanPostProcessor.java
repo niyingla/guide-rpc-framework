75896651d02ff8e348270f65b88009a5aa453832
huohuo <as1314520>
2021-10-13 14:58:40 +0800

    初始化代码

---
+++ rpc-framework-simple/src/main/java/github/javaguide/spring/SpringBeanPostProcessor.java
@@ -0,0 +1,96 @@
+package github.javaguide.spring;
+
+import github.javaguide.annotation.RpcReference;
+import github.javaguide.annotation.RpcService;
+import github.javaguide.config.RpcServiceConfig;
+import github.javaguide.extension.ExtensionLoader;
+import github.javaguide.factory.SingletonFactory;
+import github.javaguide.provider.ServiceProvider;
+import github.javaguide.provider.impl.ZkServiceProviderImpl;
+import github.javaguide.proxy.RpcClientProxy;
+import github.javaguide.remoting.transport.RpcRequestTransport;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+
+/**
+ * call this method before creating the bean to see if the class is annotated
+ *
+ * @author shuang.kou
+ * @createTime 2020年07月14日 16:42:00
+ */
+@Slf4j
+@Component
+public class SpringBeanPostProcessor implements BeanPostProcessor {
+
+    private final ServiceProvider serviceProvider;
+    private final RpcRequestTransport rpcClient;
+
+    public SpringBeanPostProcessor() {
+        this.serviceProvider = SingletonFactory.getInstance(ZkServiceProviderImpl.class);
+        this.rpcClient = ExtensionLoader.getExtensionLoader(RpcRequestTransport.class).getExtension("netty");
+    }
+
+    /**
+     * bean 对象初始化之前调用
+     * @param bean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (bean.getClass().isAnnotationPresent(RpcService.class)) {
+            log.info("[{}] is annotated with  [{}]", bean.getClass().getName(), RpcService.class.getCanonicalName());
+            // get RpcService annotation
+            RpcService rpcService = bean.getClass().getAnnotation(RpcService.class);
+            // build RpcServiceProperties
+            RpcServiceConfig rpcServiceConfig = RpcServiceConfig.builder()
+                    .group(rpcService.group())
+                    .version(rpcService.version())
+                    .service(bean).build();
+            //服务提供对象注入到zk
+            serviceProvider.publishService(rpcServiceConfig);
+        }
+        return bean;
+    }
+
+    /**
+     * bean初始化后处理
+     * @param bean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        Class<?> targetClass = bean.getClass();
+        //获取类上的所有字段
+        Field[] declaredFields = targetClass.getDeclaredFields();
+        //循环所有字段
+        for (Field declaredField : declaredFields) {
+            //判断是否有引用注解
+            RpcReference rpcReference = declaredField.getAnnotation(RpcReference.class);
+            if (rpcReference != null) {
+                //获取注解上的配置
+                RpcServiceConfig rpcServiceConfig = RpcServiceConfig.builder()
+                        .group(rpcReference.group())
+                        .version(rpcReference.version()).build();
+                //生成远程调用类
+                RpcClientProxy rpcClientProxy = new RpcClientProxy(rpcClient, rpcServiceConfig);
+                Object clientProxy = rpcClientProxy.getProxy(declaredField.getType());
+                declaredField.setAccessible(true);
+                try {
+                    declaredField.set(bean, clientProxy);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+        return bean;
+    }
+}
